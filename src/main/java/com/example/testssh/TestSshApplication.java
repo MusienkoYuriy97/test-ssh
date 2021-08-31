709b7ee425ac55b89d448749035f11e8ee0f0cba
Yuriy Musienko <97musienko@gmail.com>
2021-08-31 16:45:12 +0300

    fff

--- src/main/java/com/example/testssh/TestSshApplication.java
+++ src/main/java/com/example/testssh/TestSshApplication.java
@@ -8,7 +8,7 @@ public class TestSshApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestSshApplication.class, args);
-        int x = 141323;
+        int x = 333333;
     }
 
 }
